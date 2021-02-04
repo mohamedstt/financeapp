51d5e0d33f62788053664adf8b037aa18c82b681
mohamedstt <mohamedschof@gmail.com>
2021-02-04 17:17:14 -0300

    add init functions

--- index.js
+++ index.js
@@ -5,37 +5,60 @@ const Modal = {
   },
 };
 
-const transactions = [
-  {
-    id: 1,
-    description: "WorkAPP",
-    amount: 500000,
-    date: "21/01/2021",
-  },
-  {
-    id: 2,
-    description: "Electricity Bill",
-    amount: -20000,
-    date: "21/01/2021",
-  },
-  {
-    id: 3,
-    description: "Internet",
-    amount: -10000,
-    date: "21/01/2021",
-  },
-  {
-    id: 4,
-    description: "Wokkkkkking",
-    amount: -50000,
-    date: "21/01/2021",
-  },
-];
-
 const Transaction = {
-  incomes() {},
-  expenses() {},
-  amount() {},
+  all: [
+    {
+      description: "WorkAPP",
+      amount: 500000,
+      date: "21/01/2021",
+    },
+    {
+      description: "Electricity Bill",
+      amount: -20000,
+      date: "21/01/2021",
+    },
+    {
+      description: "Internet",
+      amount: -10000,
+      date: "21/01/2021",
+    },
+    {
+      description: "Wokkkkkking",
+      amount: -50000,
+      date: "21/01/2021",
+    },
+  ],
+  add(transaction) {
+    Transaction.all.push(transaction);
+
+    App.reload()
+
+  },
+  remove(index){
+    Transaction.all.splice(index, 1)
+    App.reload()
+  },
+  incomes() {
+    let income = 0;
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount > 0) {
+        income += transaction.amount;
+      }
+    });
+    return income;
+  },
+  expenses() {
+    let expense = 0;
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount < 0) {
+        expense += transaction.amount;
+      }
+    });
+    return expense;
+  },
+  amount() {
+    return Transaction.incomes() + Transaction.expenses();
+  },
 };
 
 const balanceCosts = {
@@ -60,6 +83,20 @@ const balanceCosts = {
         </tr>`;
     return html;
   },
+  updateBalance() {
+    document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.incomes()
+    );
+    document.getElementById("expenseDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.expenses()
+    );
+    document.getElementById("totalDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.amount()
+    );
+  },
+  clearTransactions(){
+    balanceCosts.transactionContainer.innerHTML = ""
+  }
 };
 
 const Utils = {
@@ -75,6 +112,25 @@ const Utils = {
   },
 };
 
-transactions.forEach(function (transaction) {
-  balanceCosts.addTransaction(transaction);
-});
+const Form = {
+  submit(event) {
+    console.log(event);
+  }
+}
+
+const App = {
+  init() {
+    Transaction.all.forEach((transaction) => {
+      balanceCosts.addTransaction(transaction);
+    });
+
+    balanceCosts.updateBalance()
+  },
+  reload() {
+    balanceCosts.clearTransactions()
+    App.init()
+  },
+};
+
+App.init();
+
